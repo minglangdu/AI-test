633e9a3f3855ac2f11499926cd90d05ff2dc6d97
Minglang Du <minglangdu@gmail.com>
2025-02-03 22:46:00 -0800

    make progress towards forward feed

--- ai.cpp
+++ ai.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 #include "ai.h"
 #include "constants.h"
 
 using namespace std;
 
-// to do:
+double AIH::accs(double wsum) {
+    return wsum / (1 + abs(wsum));
+}
+
+AIH::Neuron::Neuron(int wsize, double bi) {
+    weights = vector<double> (wsize);
+    bias = bi;
+    value = 0;
+}
+
+vector<vector<double>> AIH::Layer::showVal() {
+    vector<vector<double>> res;
+    for (Neuron* n : neurons) {
+        res.push_back(vector<double> (1, n->value));
+    }
+    return res;
+}
+
+vector<vector<double>> AIH::Layer::showWM() {
+    // to do
+}
+
+AIH::Layer::Layer(Layer* prevl, int nexsize, int size) {
+    neurons = vector<Neuron*> ();
+    for (int i = 0; i < size; i ++) {
+        neurons.push_back(new Neuron(nexsize, 0));
+    }
+    prev = prevl;
+}
+
 AIH::Network::Network() {
+    // random test network
     
 }
--- ai.h
+++ ai.h
@@ -6,21 +6,42 @@
 #include "constants.h"
 
 namespace AIH {
+    // Is an accelerated version of the sigmoid function.
+    double accs(double wsum); 
     struct Neuron {
-        std::vector<double> weights;
+        // the weights on the connections to other neurons
+        std::vector<double> weights; 
         double value;
+        // subtract this from the weighted sum
+        double bias;
         public:
+            Neuron(int wsize, double bi);
             friend struct Layer;
             friend class Network;
     };
     struct Layer {
         std::vector<Neuron*> neurons;
+        Layer* prev;
         public:
             friend struct Neuron;
             friend class Network;
+            Layer(Layer* prevl, int nexsize, int size);
+            std::vector<std::vector<double>> showVal(); // shows the values of all the neurons in a one-column matrix
+            std::vector<std::vector<double>> showWM(); // shows weight matrix as described below
+            std::vector<Neuron*> getVal(); // forward feed
+            /*
+            Use matrix multiplication: 
+            Get a matrix w where one row is all the connections of the neurons
+            of the prev layer with one neuron in the current layer.
+            This matrix can be gotten with the showWM function.
+            Then, get a one-column matrix a where the neurons correspond to the previous layer's values. 
+            This one-column matrix can be gotten with the showVal function.
+            */
     };
     class Network {
+        std::vector<Layer*> layers;
         public:
             Network();
+            void feedforward();
     };
 }
